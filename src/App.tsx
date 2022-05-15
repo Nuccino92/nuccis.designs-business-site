6f152a424926aa0571c2e5d6c8947710627e1827
Nuccino92 <anthnucci@hotmail.com>
2022-05-15 02:24:24 -0400

    implemented full react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import "./App.scss";
+import Homepage from "./pages/homepage/Homepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Services from "./pages/services/Services";
+import AboutMe from "./pages/aboutMe/AboutMe";
+import Clients from "./pages/clients/Clients";
+import Contact from "./pages/contact/Contact";
+import PaymentsAndReturnPolicy from "./pages/paymentsAndReturnPolicy/PaymentsAndReturnPolicy";
+import ProductDeliveryAndReturns from "./pages/productDelivery/ProductDelivery";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
-function App() {
-  return <div className="App">app</div>;
-}
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="aboutme" element={<AboutMe />} />
+            <Route path="clients" element={<Clients />} />
+            <Route
+              path="payments-refunds"
+              element={<PaymentsAndReturnPolicy />}
+            />
+            <Route
+              path="productdelivery"
+              element={<ProductDeliveryAndReturns />}
+            />
+          </Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
