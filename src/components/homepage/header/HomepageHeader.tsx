d2ae47365136dd7ab6511636e82e8915c5f42f6b
Nuccino92 <anthnucci@hotmail.com>
2022-05-16 23:16:52 -0400

    full hompeage header

--- src/components/homepage/header/HomepageHeader.tsx
+++ src/components/homepage/header/HomepageHeader.tsx
@@ -1,11 +1,17 @@
 import "./HomepageHeader.scss";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
 const HomepageHeader = () => {
   return (
     <Container className="HomepageHeader">
       <h1>Nuccis Designs</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
     </Container>
   );
 };
