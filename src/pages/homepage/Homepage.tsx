2922aae3b387a279133645e5286b499650916f8e
Nuccino92 <anthnucci@hotmail.com>
2022-05-16 23:16:09 -0400

    homepage component

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,26 @@
+import { Helmet } from "react-helmet";
+import HomepageAboutMe from "../../components/homepage/aboutMe/HomePageAboutMe";
+import HomepageCategories from "../../components/homepage/categories/HomepageCategories";
+import HomepageClients from "../../components/homepage/clients/HomepageClients";
+import HomepageContact from "../../components/homepage/contact/HomepageContact";
+import HomepageHeader from "../../components/homepage/header/HomepageHeader";
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+      {/* <Helmet>
+        <title>smelly bean</title>
+        <meta name="description" content=""></meta>
+      </Helmet> */}
+      <header>
+        <HomepageHeader />
+      </header>
+      <HomepageCategories />
+      <HomepageAboutMe />
+      <HomepageClients />
+      <HomepageContact />
+    </div>
+  );
+};
+
+export default Homepage;
