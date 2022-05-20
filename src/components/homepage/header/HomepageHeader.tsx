4c7e682ba9de6687f81d61e8d87c965ca1eda867
Nuccino92 <anthnucci@hotmail.com>
2022-05-20 16:52:51 -0400

    edited header paragraph

--- src/components/homepage/header/HomepageHeader.tsx
+++ src/components/homepage/header/HomepageHeader.tsx
@@ -1,17 +1,13 @@
 import "./HomepageHeader.scss";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import cover from "./coverImage.jpg";
 
 const HomepageHeader = () => {
   return (
     <Container className="HomepageHeader">
       <Container>
         <h1>Nuccis Designs</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod
-        </p>
+        <p>Custom designed products that are made for you. </p>
         <Link to="/services">Services</Link>
       </Container>
       <div className="HomepageHeader-background"></div>
