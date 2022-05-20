1e10892c7605f34040694d1b2d0ce92bc94c4510
Nuccino92 <anthnucci@hotmail.com>
2022-05-20 16:54:22 -0400

    completed homepage clients section

--- src/components/homepage/clients/HomepageClients.tsx
+++ src/components/homepage/clients/HomepageClients.tsx
@@ -1,7 +1,24 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./HomepageClients.scss";
+import ClientsPhoto from "./images/clients.jpg";
 
 const HomepageClients = () => {
-  return <div>clients</div>;
+  return (
+    <Container className="HomepageClients">
+      <Row>
+        <Col>
+          <Link to="/clients">Reviews / Clients</Link>
+        </Col>
+        <Col>
+          <img
+            src={ClientsPhoto}
+            alt="Clients holding up products made for them"
+          ></img>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default HomepageClients;
