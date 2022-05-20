b28e0f65206f2c11ba3e89694e0e957cc85d0995
Nuccino92 <anthnucci@hotmail.com>
2022-05-19 23:12:14 -0400

    completed homepage about me section

--- src/components/homepage/aboutMe/HomePageAboutMe.tsx
+++ src/components/homepage/aboutMe/HomePageAboutMe.tsx
@@ -1,7 +1,21 @@
 import "./HomepageAboutMe.scss";
+import { Col, Container, Row } from "react-bootstrap";
+import MyPicture from "./images/Me.jpg";
+import { Link } from "react-router-dom";
 
 const HomepageAboutMe = () => {
-  return <div>about me</div>;
+  return (
+    <Container className="HomepageAboutMe">
+      <Row>
+        <Col>
+          <img src={MyPicture} alt="Me blowing out candles"></img>
+        </Col>
+        <Col>
+          <Link to="/aboutme">Get to know me</Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default HomepageAboutMe;
