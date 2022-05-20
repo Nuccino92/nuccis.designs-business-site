c8d961a4ebc901b929fd468c574f38581ba90bc1
Nuccino92 <anthnucci@hotmail.com>
2022-05-20 18:43:18 -0400

    updated cover images and added sign category

--- src/components/homepage/categories/HomepageCategories.tsx
+++ src/components/homepage/categories/HomepageCategories.tsx
@@ -8,6 +8,7 @@ import clothingCover from "../../images/coverImages/clothing.jpg";
 import homeDecorCover from "../../images/coverImages/homeDecor.jpg";
 import invitationCover from "../../images/coverImages/invitation.jpg";
 import petCover from "../../images/coverImages/pet.jpg";
+import signCover from "../../images/coverImages/sign.jpg";
 
 // view more images
 import TeddyBear from "../../images/viewMoreImages/TeddyBear.jpg";
@@ -20,33 +21,21 @@ const HomepageCategories = () => {
       <h2>Check out some popular product categories</h2>
       <Row>
         <Col>
-          <img
-            className="home-decor-cover"
-            src={homeDecorCover}
-            alt="Pillows with custom print"
-          ></img>
+          <img src={homeDecorCover} alt="Pillows with custom print"></img>
           <Container>
             <h4>Home Decor</h4>
             <button>View More</button>
           </Container>
         </Col>
         <Col>
-          <img
-            className="pet-cover"
-            src={petCover}
-            alt="Dog with mug of decal of it"
-          ></img>
+          <img src={petCover} alt="Dog with mug of decal of it"></img>
           <Container>
             <h4>Pets</h4>
             <button>View More</button>
           </Container>
         </Col>
         <Col>
-          <img
-            className="invitations-cover"
-            src={invitationCover}
-            alt="Custom invitations"
-          ></img>
+          <img src={invitationCover} alt="Custom invitations"></img>
           <Container>
             <h4>Invitations</h4>
             <button>View More</button>
@@ -55,30 +44,30 @@ const HomepageCategories = () => {
       </Row>
       <Row>
         <Col>
-          <img
-            className="clothing-cover"
-            src={clothingCover}
-            alt="Custom t-shirts"
-          ></img>
+          <img src={clothingCover} alt="Custom t-shirts"></img>
           <Container>
             <h4>Clothing</h4>
             <button>View More</button>
           </Container>
         </Col>
         <Col>
-          <img
-            className="bridal-cover"
-            src={bridalCover}
-            alt="Bridal party carepackage"
-          ></img>
+          <img src={bridalCover} alt="Bridal party carepackage"></img>
           <Container>
             <h4>Bridal Designs</h4>
             <button>View More</button>
           </Container>
         </Col>
+        <Col>
+          <img src={signCover} alt="Custom t-shirts"></img>
+          <Container>
+            <h4>Signs</h4>
+            <button>View More</button>
+          </Container>
+        </Col>
       </Row>
+
       <Container className="view-more-categories">
-        <h4>VIEW MORE DESIGNS AND A FULL BREAKDOWN OF MY SERVICES</h4>
+        <h4>View more designs and a full breakdown of my services</h4>
         <Link to="/services">
           <button>Click Here</button>
         </Link>
