bf2ef53e11bd31cab40e66a7c66107b75f15f8b6
Nuccino92 <anthnucci@hotmail.com>
2022-05-22 23:48:13 -0400

    full service page

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,5 +1,49 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ServicesCategories from "../../components/services/serviceCategories/ServicesCategories";
+import "./Services.scss";
+import { Fade } from "react-awesome-reveal";
+
 const Services: React.FC = () => {
-  return <div>services</div>;
+  return (
+    <Container className="Services">
+      {/* @ts-ignore: Unreachable code error  */}
+      <Fade>
+        <Container>
+          <h1>About My Services</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+            quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+            mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+            Vestibulum lacinia arcu eget nulla.
+          </p>
+          <Row>
+            <Col>
+              <h4>Links to answer a few of your questions </h4>
+            </Col>
+            <Col>
+              <Link to="/payments-refunds">Payments</Link>
+              <Link to="/payments-refunds">Refuned Policy</Link>
+              <Link to="/productdelivery">Product Delivery</Link>
+              <Link to="/productdelivery">Returns / Changes</Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h4>
+                To move forward or if you have any other questions visit
+                <Link to="/contact">Contact Page</Link>
+              </h4>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </Fade>
+
+      <ServicesCategories />
+    </Container>
+  );
 };
 
 export default Services;
