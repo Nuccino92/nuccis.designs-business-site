fc4e5e7aa300544baf5cab78ab2ab7d946ea722a
Nuccino92 <anthnucci@hotmail.com>
2022-05-22 23:46:44 -0400

    added link to each category

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,18 @@ import ProductDeliveryAndReturns from "./pages/productDelivery/ProductDelivery";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
+// service pages
+import HomeDecor from "./pages/category/homeDecor/HomeDecor";
+import Pets from "./pages/category/pets/Pets";
+import Clothing from "./pages/category/clothing/Clothing";
+import Invitations from "./pages/category/invitations/Invitations";
+import Bridal from "./pages/category/bridal/Bridal";
+import Childern from "./pages/category/children/Children";
+import Signs from "./pages/category/signs/Signs";
+import Festive from "./pages/category/festive/Festive";
+import Teacher from "./pages/category/teacher/Teacher";
+import Decals from "./pages/category/decals/Decals";
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
@@ -18,17 +30,29 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/services" element={<Services />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="aboutme" element={<AboutMe />} />
-            <Route path="clients" element={<Clients />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/aboutme" element={<AboutMe />} />
+            <Route path="/clients" element={<Clients />} />
             <Route
-              path="payments-refunds"
+              path="/payments-refunds"
               element={<PaymentsAndReturnPolicy />}
             />
             <Route
-              path="productdelivery"
+              path="/productdelivery"
               element={<ProductDeliveryAndReturns />}
             />
+
+            {/* service category routes */}
+            <Route path="/services/homedecor" element={<HomeDecor />} />
+            <Route path="/services/pets" element={<Pets />} />
+            <Route path="/services/clothings" element={<Clothing />} />
+            <Route path="/services/invitations" element={<Invitations />} />
+            <Route path="/services/bridal" element={<Bridal />} />
+            <Route path="/services/children" element={<Childern />} />
+            <Route path="/services/signs" element={<Signs />} />
+            <Route path="/services/festive" element={<Festive />} />
+            <Route path="/services/teacher" element={<Teacher />} />
+            <Route path="/services/decals" element={<Decals />} />
           </Routes>
         </main>
         <footer>
