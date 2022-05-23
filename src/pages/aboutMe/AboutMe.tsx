86fecf07f8b0dc4b758b4b4a0f9a2ce393198404
Nuccino92 <anthnucci@hotmail.com>
2022-05-22 23:48:01 -0400

    about me page

--- src/pages/aboutMe/AboutMe.tsx
+++ src/pages/aboutMe/AboutMe.tsx
@@ -1,5 +1,37 @@
+import { Fade } from "react-awesome-reveal";
+import { Col, Container, Row } from "react-bootstrap";
+import "./AboutMe.scss";
+import familyPhoto from "./images/family.jpg";
+
 const AboutMe: React.FC = () => {
-  return <div>about me</div>;
+  return (
+    <Container className="AboutMe">
+      {/* @ts-ignore: Unreachable code error  */}
+      <Fade triggerOnce>
+        <Row>
+          <Col>
+            <div className="frame">
+              <img src={familyPhoto} alt="Me and my fiance by our new home" />
+            </div>
+          </Col>
+          <Col>
+            <h1>My Story</h1>
+            <p>
+              Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in
+              justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus
+              luctus magna. Quisque cursus, metus vitae pharetra auctor, sem
+              massa mattis sem, at interdum magna augue eget diam. Vestibulum
+              ante ipsum primis in faucibus orci luctus et ultrices posuere
+              cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor.
+              Sed non quam. In vel mi sit amet augue congue elementum. Morbi in
+              ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra
+              nec.
+            </p>
+          </Col>
+        </Row>
+      </Fade>
+    </Container>
+  );
 };
 
 export default AboutMe;
