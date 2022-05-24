e498af92d1ec7f95e898ff6b03305922bceaaad2
Nuccino92 <anthnucci@hotmail.com>
2022-05-23 23:48:55 -0400

    added dropdown for the nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,9 +6,19 @@ import useWindowSize from "../../hooks/useWindowSize";
 import "./Navbar.scss";
 import { Fade } from "react-awesome-reveal";
 
+import { AiFillCaretDown } from "react-icons/ai";
+
 const Navbar: React.FC = () => {
   const [minimized, setMinimized] = useState<boolean>(false);
   const [mobileNav, setMobileNav] = useState<boolean>(false);
+
+  const [dropdown, setDropdown] = useState<boolean>(false);
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setDropdown(false);
+    }, 100);
+  };
 
   let windowSize = useWindowSize();
 
@@ -30,6 +40,51 @@ const Navbar: React.FC = () => {
               <Link to="/contact">CONTACT</Link>
               <Link to="/aboutme">ABOUT ME</Link>
               <Link to="/clients">CLIENTS</Link>
+              <ul className="Navbar-dropdown" onBlur={handleBlur}>
+                <button
+                  onClick={() =>
+                    setDropdown((prev) => {
+                      return !prev;
+                    })
+                  }
+                >
+                  PRODUCTS <AiFillCaretDown size={19} />
+                </button>
+                {dropdown && (
+                  <Container>
+                    <Fade>
+                      <Link to="/services/bridal">Bridal</Link>
+                    </Fade>
+                    <Fade delay={200}>
+                      <Link to="/services/homedecor">Home Decor</Link>
+                    </Fade>
+                    <Fade delay={250}>
+                      <Link to="/services/pets">Pets</Link>
+                    </Fade>
+                    <Fade delay={300}>
+                      <Link to="/services/clothing">Clothing</Link>
+                    </Fade>
+                    <Fade delay={350}>
+                      <Link to="/services/children">Children</Link>
+                    </Fade>
+                    <Fade delay={400}>
+                      <Link to="/services/festive">Festive</Link>
+                    </Fade>
+                    <Fade delay={450}>
+                      <Link to="/services/teacher">Teacher</Link>
+                    </Fade>
+                    <Fade delay={500}>
+                      <Link to="/services/decals">Decals</Link>
+                    </Fade>
+                    <Fade delay={550}>
+                      <Link to="/services/invitations">Invitations</Link>
+                    </Fade>
+                    <Fade delay={600}>
+                      <Link to="/services/signs">Signs</Link>
+                    </Fade>
+                  </Container>
+                )}
+              </ul>
             </Nav>
           </Container>
         </BoostrapNavbar>
