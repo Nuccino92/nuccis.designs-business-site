f0670b7d61154398a6c05705f360fc0de471ca55
Nuccino92 <anthnucci@hotmail.com>
2022-05-23 23:50:54 -0400

    added styled component buttons

---
+++ src/styledComponents/FacebookReviewButton.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FacebookReviewButton = styled.button`
+  background-color: rgb(66, 103, 178);
+  color: white;
+  font-size: 22px;
+  font-weight: bold;
+  padding: 15px;
+  cursor: pointer;
+  border-radius: 4px;
+`;
+
+export default FacebookReviewButton;
---
+++ src/styledComponents/StoryButton.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const StoryButton = styled.button`
+  padding: 15px;
+  color: white;
+  cursor: pointer;
+  font-size: 22px;
+  font-weight: bold;
+  border-radius: 4px;
+  background: #f09433;
+  background: -moz-linear-gradient(
+    45deg,
+    #f09433 0%,
+    #e6683c 25%,
+    #dc2743 50%,
+    #cc2366 75%,
+    #bc1888 100%
+  );
+  background: -webkit-linear-gradient(
+    45deg,
+    #f09433 0%,
+    #e6683c 25%,
+    #dc2743 50%,
+    #cc2366 75%,
+    #bc1888 100%
+  );
+  background: linear-gradient(
+    45deg,
+    #f09433 0%,
+    #e6683c 25%,
+    #dc2743 50%,
+    #cc2366 75%,
+    #bc1888 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
+`;
+
+export default StoryButton;
