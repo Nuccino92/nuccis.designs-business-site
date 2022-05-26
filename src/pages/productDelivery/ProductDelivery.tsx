17ecf7ee6318c972703b92d167d29d4b649ffce9
Nuccino92 <anthnucci@hotmail.com>
2022-05-25 23:54:07 -0400

    full product delivery page

--- src/pages/productDelivery/ProductDelivery.tsx
+++ src/pages/productDelivery/ProductDelivery.tsx
@@ -1,5 +1,36 @@
-const ProductDeliveryAndReturns: React.FC = () => {
-  return <div>Delivery and returns</div>;
+import { Fade } from "react-awesome-reveal";
+import { Container, Row, Col } from "react-bootstrap";
+import "./ProductDelivery.scss";
+
+const ProductDelivery: React.FC = () => {
+  return (
+    <Container className="ProductDelivery">
+      {/* @ts-ignore: Unreachable code error  */}
+      <Fade triggerOnce>
+        <h1>Product Delivery</h1>
+        <Row>
+          <Col>
+            <h4>Delivery & Shipping</h4>
+            <p>
+              We deliver to Toronto, Peel, Simcoe & York Region. Shipping or
+              delivering the product is an additional fee.
+            </p>
+          </Col>
+          <Col>
+            <h4>Product Pickup</h4>
+            <p>
+              Pickup locations are in Aurora, ON Sunday through Saturday all
+              hours & Vaughan, ON Monday through Friday 9 a.m. - 4 p.m.
+            </p>
+          </Col>
+          <Col>
+            <h4>Halfway Service</h4>
+            <p>We offer a meet half way delivery service.</p>
+          </Col>
+        </Row>
+      </Fade>
+    </Container>
+  );
 };
 
-export default ProductDeliveryAndReturns;
+export default ProductDelivery;
