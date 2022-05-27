0d9f2f8ff892e195c8b89e4b382d82c5cd0c4eb9
Nuccino92 <anthnucci@hotmail.com>
2022-05-26 21:23:55 -0400

    completed contact page description

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -13,13 +13,21 @@ const Contact: React.FC = () => {
           <Col>
             <h1>Our Process</h1>
             <p>
-              Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem,
-              at interdum magna augue eget diam. Vestibulum ante ipsum primis in
-              faucibus orci luctus et ultrices posuere cubilia Curae; Morbi
-              lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel
-              mi sit amet augue congue elementum. Morbi in ipsum sit amet pede
-              facilisis laoreet. Donec lacus nunc, viverra nec.
+              If you would like to place an order or get in touch for assistance
+              on what you would like done you may send me an email. I will
+              respond as soon as possible and then after we can get your order
+              ready. Once payment has been made, your order will be processed
+              and worked on until completion. Get started by sending an email to{" "}
+              <span>{process.env.REACT_APP_EMAIL}</span>.
             </p>
+
+            <ul>
+              <h3>Steps</h3>
+              <li>1. Contact to create an order</li>
+              <li>2. Make payment</li>
+              <li>3. The order is processed</li>
+              <li>4. Product is delivered</li>
+            </ul>
 
             <h4>Links to answer a few of your questions </h4>
 
