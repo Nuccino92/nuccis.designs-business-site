21a9422586869334b8cf7fb47262a74515a7a902
Nuccino92 <anthnucci@hotmail.com>
2022-05-27 21:18:09 -0400

    .

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 import HomepageAboutMe from "../../components/homepage/aboutMe/HomePageAboutMe";
 import HomepageCategories from "../../components/homepage/categories/HomepageCategories";
 import HomepageClients from "../../components/homepage/clients/HomepageClients";
