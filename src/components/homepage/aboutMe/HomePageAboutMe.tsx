a6916ab652252b2f67a2f51b08b0289b4dc1e987
Nuccino92 <anthnucci@hotmail.com>
2022-06-01 19:08:24 -0400

    adjusted homepage about me

--- src/components/homepage/aboutMe/HomePageAboutMe.tsx
+++ src/components/homepage/aboutMe/HomePageAboutMe.tsx
@@ -11,10 +11,17 @@ const HomepageAboutMe = () => {
       <Fade triggerOnce duration={2000}>
         <Row>
           <Col>
-            <img src={MyPicture} alt="Me blowing out candles"></img>
+            <img src={MyPicture} alt="Me"></img>
           </Col>
           <Col>
             <Link to="/aboutme">Get to know me</Link>
+            <p>
+              I'm passionate about what I do. If you wish to know a little bit
+              about me or my story, head over to my about me page!
+            </p>
+            <button>
+              <Link to="/aboutme">About Me</Link>
+            </button>
           </Col>
         </Row>
       </Fade>
